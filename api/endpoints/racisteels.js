e1f74ac1e61903c763924f5e8db853755c989088
Drew Lemmy <lemonplays@gmail.com>
2016-01-10 18:48:10 +0000

    Added important features

--- api/endpoints/racisteels.js
+++ api/endpoints/racisteels.js
@@ -1,3 +1,5 @@
+var crc32 = require('crc-32');
+
 var EndpointHandler = require('../endpoint_handler.js');
 
 function RacistEels() {
@@ -6,7 +8,44 @@ function RacistEels() {
 	racistEels.prototype = EndpointHandler();
 
 	racistEels.handle = function(app, req, res, val) {
-		res.send("FUCKERY" + val);
+		var positive = [
+			'Yes.',
+			'Certainly.',
+			'Of course!',
+			'Without a doubt.',
+			'Most likely.'
+		];
+
+		var negative = [
+			'No.',
+			'Probably not.',
+			'No way!',
+			'Nay!',
+			'Nope.'
+		];
+
+		var questionWords = [
+			'is',
+			'can',
+			'are',
+			'has'
+		];
+
+		// are you prepared for a clusterfuck of oneliners?
+
+		if ((val = val.toLowerCase()).trim().substr(-1) === '?') {
+			if (questionWords.indexOf(val.split(' ')[0]) >= 0) {
+				if (crc32.str(val.replace(/[^a-z0-9]/gi, '')) % 2 == 0) {
+					res.send(positive[Math.floor(Math.random()*positive.length)]);
+				} else {
+					res.send(negative[Math.floor(Math.random()*negative.length)]);
+				}
+			} else {
+				res.send('We don\'t know.');
+			}
+		} else {
+			res.send('Ask us a question. Disclaimer: we are partially human aided.');
+		}
 	};
 
 	racistEels.setup = function(app) {
