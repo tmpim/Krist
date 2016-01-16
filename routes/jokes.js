a8fa294acb65c24f8a526653ffadec39bf5b5989
Drew Lemmy <lemonplays@gmail.com>
2016-01-16 19:05:51 +0000

    Fulfilled cdels request

---
+++ routes/jokes.js
@@ -0,0 +1,11 @@
+var krist = require('./../src/krist.js');
+
+module.exports = function(app) {
+	app.get('/cdel', function(req, res) {
+		res.send("Connor dominates and penetrates Ryan Smith.");
+	});
+	
+	app.get('/eskreme', function(req, res) {
+		res.send("Connor dominates and penetrates Ryan Smith.");
+	});
+}
