764ee81d74e82aa6c1de2e37371063e90c1cc685
Drew Lemmy <lemonplays@gmail.com>
2016-02-03 01:04:23 +0000

    Added motd to the docs

--- src/routes/motd.js
+++ src/routes/motd.js
@@ -3,6 +3,26 @@ var krist   = require('./../krist.js'),
 	moment  = require('moment');
 
 module.exports = function(app) {
+	/**
+	 * @api {get} /motd Get the message of the day
+	 * @apiName GetMOTD
+	 * @apiGroup MiscellaneousGroup
+	 * @apiVersion 2.0.0
+	 *
+	 * @apiSuccess {String} motd The message of the day
+	 * @apiSuccess {String} psa A public service announcement if there is one
+	 * @apiSuccess {String} schrodingers_cat The current observed state of Schrödinger's cat
+	 * @apiSuccess {Date} set The date the MOTD was last changed
+	 *
+	 * @apiSuccessExample {json} Success
+	 * {
+	 *     "ok": true,
+	 *     "motd": "Welcome to Krist!",
+	 *     "psa": "child abuse is bad!!",
+	 *     "schrodingers_cat": "dead",
+	 *     "set": "2016-01-24T15:56:19.231Z"
+	 * }
+	 */
 	app.all('/motd', function(req, res) {
 		fs.readFile('motd.txt', function(err, data) {
 			if (err) {
