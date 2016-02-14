6564ec0c7281049e254a4f8543122dee85179a4c
Drew Lemmy <drew@lemmmy.pw>
2016-02-14 03:32:15 +0000

    Fixed another error in webhook apidoc

--- src/routes/webhooks.js
+++ src/routes/webhooks.js
@@ -68,7 +68,7 @@ module.exports = function(app) {
 	 * @apiParam (BodyParameter) {String="transaction","block","name"} event The event type of this webhook.
 	 * @apiParam (BodyParameter) {String} url The URL to call for this webhook.
 	 * @apiParam (BodyParameter) {String="get","post"} [method] The HTTP method to call for this webhook.
-	 * @apiParam (BodyParameter) {String) [addresses] A comma delimited list of addresses to whitelist for this event.
+	 * @apiParam (BodyParameter) {String} [addresses] A comma delimited list of addresses to whitelist for this event.
 	 * 			 Only valid for `transaction` or `name` events.
 	 *
 	 * @apiUse Webhook
