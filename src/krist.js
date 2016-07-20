70b2405a14d9a4cca23b6a76ca5eb8a0e4932f8a
Drew Lemmy <drew@lemmmy.pw>
2016-07-20 19:15:24 +0100

    maybe fixed a records

--- src/krist.js
+++ src/krist.js
@@ -36,7 +36,7 @@ var utils       = require('./utils.js'),
 var addressRegex = /^(?:k[a-z0-9]{9}|[a-f0-9]{10})$/i;
 var addressListRegex = /^(?:k[a-z0-9]{9}|[a-f0-9]{10})(?:,(?:k[a-z0-9]{9}|[a-f0-9]{10}))*$/i;
 var nameRegex = /^[a-z0-9]{1,64}$/i;
-var aRecordRegex = /^[^\s/$.?#].[^\s]*$/i;
+var aRecordRegex = /^[^\s\$\.\?\#].[^\s]*$/i;
 
 Krist.work = 18750; // work as of the writing of this line. this is used purely for backup.
 Krist.freeNonceSubmission = false;
