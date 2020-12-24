c545ded4bff000f9b0c41d350a07be7a780438e7
Drew Lemmy <drew@lemmmy.pw>
2020-12-24 22:10:26 +0000

    fix: add redis password

--- src/redis.js
+++ src/redis.js
@@ -11,12 +11,14 @@ module.exports = {
   init() {
     const host = process.env.REDIS_HOST || "127.0.0.1";
     const port = parseInt(process.env.REDIS_PORT) || 6379;
+    const password = process.env.REDIS_PASSWORD || undefined;
     const prefix = process.env.REDIS_PREFIX || "krist:";
     
     console.log(chalk.blue("Connecting to redis"));
     redis = createNodeRedisClient({
       host,
       port,
+      password,
       prefix
     });
     console.log(chalk.green("Connected to redis"));
