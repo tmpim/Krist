4ebd36c28a9b6511e29198514e6840038d9f37ea
Drew Lemmy <drew@lemmmy.pw>
2021-02-06 17:32:28 +0000

    feat: add constants and currency to /motd

--- src/routes/motd.js
+++ src/routes/motd.js
@@ -20,6 +20,7 @@
  */
 
 const krist = require("./../krist.js");
+const constants = require("./../constants.js");
 
 module.exports = function(app) {
   /**
@@ -32,13 +33,58 @@ module.exports = function(app) {
 	 * @apiSuccess {Date} set The date the MOTD was last changed
 	 * @apiSuccess {Boolean} debug_mode If the server is running in debug mode,
    *                       this will be set to 'true'.
+   * 
+	 * @apiSuccess {Object} constants Constants related to the Krist server
+   *    configuration.
+   * @apiSuccess {Number} constants.wallet_version The latest version of
+   *    KristWallet.
+   * @apiSuccess {Number} constants.nonce_max_size The maximum size, in bytes,
+   *    of a block nonce.
+   * @apiSuccess {Number} constants.name_cost The cost, in KST, of purchasing
+   *    a new name.
+   * @apiSuccess {Number} constants.min_work The minimum work (block difficulty)
+   *    value. The work will not automatically go below this.
+   * @apiSuccess {Number} constants.max_work The maximum work (block difficulty)
+   *    value. The work will not automatically go above this.
+   * @apiSuccess {Number} constants.work_factor The factor at which the work
+   *    may change per block.
+   * @apiSuccess {Number} constants.seconds_per_block The ideal time between
+   *    mined blocks. The Krist server will adjust the difficulty to match this
+   *    value.
+   * 
+	 * @apiSuccess {Object} currency Constants related to the currency that this
+   *    server represents.
+   * @apiSuccess {String} currency.address_prefix The character that each
+   *    address starts with (e.g. `k`).
+   * @apiSuccess {String} currency.name_suffix The suffix that each name ends
+   *    with after the dot (e.g. `kst`)
+   * @apiSuccess {String} currency.currency_name The full long name of this
+   *    currency (e.g. `Krist`).
+   * @apiSuccess {String} currency.currency_symbol The shorthand symbol for this
+   *    currency (e.g. `KST`).
 	 *
 	 * @apiSuccessExample {json} Success
-	 * {
-	 *     "ok": true,
-	 *     "motd": "Welcome to Krist!",
-	 *     "set": "2016-01-24T15:56:19.231Z"
-	 * }
+   * {
+   *   "ok": true,
+   *   "motd": null,
+   *   "set": "2021-01-01T00:00:00.000Z",
+   *   "debug_mode": true,
+   *   "constants": {
+   *     "wallet_version": 16,
+   *     "nonce_max_size": 24,
+   *     "name_cost": 500,
+   *     "min_work": 100,
+   *     "max_work": 100000,
+   *     "work_factor": 0.025,
+   *     "seconds_per_block": 60
+   *   },
+   *   "currency": {
+   *     "address_prefix": "k",
+   *     "name_suffix": "kst",
+   *     "currency_name": "Krist",
+   *     "currency_symbol": "KST"
+   *   }
+   * }
 	 */
   app.all("/motd", async function(req, res) {
     const { motd, motd_set, debug_mode } = await krist.getMOTD();
@@ -47,7 +93,26 @@ module.exports = function(app) {
       ok: true,
       motd,
       set: motd_set,
-      debug_mode
+
+      debug_mode,
+
+      constants: {
+        wallet_version: constants.walletVersion,
+        nonce_max_size: constants.nonceMaxSize,
+        name_cost: constants.nameCost,
+        min_work: constants.minWork,
+        max_work: constants.maxWork,
+        work_factor: constants.workFactor,
+        seconds_per_block: constants.secondsPerBlock
+      },
+
+      currency: {
+        address_prefix: "k",
+        name_suffix: "kst",
+  
+        currency_name: "Krist",
+        currency_symbol: "KST"
+      }
     });
   });
 
